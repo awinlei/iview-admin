a5efd0e453eb9af723ea0fb18199b1ce2a4102a2
wanglilei <awinlei@gmail.com>
2017-11-24 17:16:01 +0800

    单图表，多页签图表处理方式

--- src/views/sumary/components/keyRequests.vue
+++ src/views/sumary/components/keyRequests.vue
@@ -1,92 +1,198 @@
+<style>
+
+</style>
 <template>
-    <Tabs type="card" closable @on-tab-remove="handleTabRemove">
-        <TabPane label="新增激活和账户" v-if="tab0"><div style="width:100%;height:100%;" id="key_request_con"></div></TabPane>
-        <TabPane label="活跃玩家" v-if="tab1"><div style="width:100%;height:100%;" id="key_request_con1"></div></TabPane>
-        <TabPane label="付费玩家" v-if="tab2"><div style="width:100%;height:100%;" id="key_request_con2"></div></TabPane>
-        <TabPane label="收入" v-if="tab2"><div style="width:100%;height:100%;" id="key_request_con3"></div></TabPane>
-    </Tabs>
+    <div class="echart_tab_style">
+        <Tabs type="card" :animated="true" @on-click="handleTabChange">
+            <TabPane label="新增激活和账户" v-if="tab0"><div style="width:100%;height:100%;" id="key_request_con"></div></TabPane>
+            <TabPane label="活跃玩家" v-if="tab1"><div style="width:100%;height:100%;" id="key_request_con1"></div></TabPane>
+            <TabPane label="付费玩家" v-if="tab2"><div style="width:100%;height:100%;" id="key_request_con2"></div></TabPane>
+            <TabPane label="收入" v-if="tab3"><div style="width:100%;height:100%;" id="key_request_con3"></div></TabPane>
+        </Tabs>
+    </div>
 </template>
 
 <script>
-const echarts = require('echarts');
-export default {
-    name: 'keyRequests',
-    data () {
-            return {
-                tab0: true,
-                tab1: true,
-                tab2: true
-            }
-        },
-    methods: {
-            handleTabRemove (name) {
-                console.log(name);
-                this['tab' + name] = false;
-            }
-        },
-    mounted () {
-        const option = {
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'cross',
-                    label: {
-                        backgroundColor: '#6a7985'
-                    }
-                }
-            },
-            grid: {
-                top: '3%',
-                left: '1.2%',
-                right: '1%',
-                bottom: '3%',
-                containLabel: true
-            },
-            xAxis: [
-                {
-                    type: 'category',
-                    boundaryGap: false,
-                    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-                }
-            ],
-            yAxis: [
-                {
-                    type: 'value'
-                }
-            ],
-            series: [
-                {
-                    name: '银行/证券',
-                    type: 'line',
-                    stack: '总量',
-                    areaStyle: {normal: {
-                        color: '#10A6FF'
-                    }},
-                    data: [257, 358, 278, 234, 290, 330, 310]
-                },
-                {
-                    name: '快递/电商',
-                    type: 'line',
-                    stack: '总量',
-                    label: {
-                        normal: {
-                            show: true,
-                            position: 'top'
-                        }
-                    },
-                    areaStyle: {normal: {
-                        color: '#398DBF'
-                    }},
-                    data: [820, 645, 546, 745, 872, 624, 258]
-                }
-            ]
-        };
-        const keyRequestCharts = echarts.init(document.getElementById('key_request_con'));
-        keyRequestCharts.setOption(option);
+const echarts = require("echarts");
+require("@/libs/echarts/macarons");
+import util from "@/libs/util";
 
-        window.addEventListener('resize', function () {
-            keyRequestCharts.resize();
-        });
+export default {
+  name: "keyRequests",
+  data() {
+    return {
+      tab0: true,
+      tab1: true,
+      tab2: true,
+      tab3: true,
+      initTab: 'key_request_con',
+      chartObj: null,
+      optionLenged: ["ACU", "PCU"],
+      optionXAxis: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+      optionyAxis: "value",
+      optionData: [
+        {
+          name: "ACU",
+          type: "line",
+          stack: "总量",
+          itemStyle: { normal: { areaStyle: { type: "default" } } },
+          data: []
+        },
+        {
+          name: "PCU",
+          type: "line",
+          stack: "总量",
+          label: {
+            normal: {
+              show: true,
+              position: "top"
+            }
+          },
+          itemStyle: { normal: { areaStyle: { type: "default" } } },
+          data: [],
+          markPoint: {
+            data: [{ type: "max", name: "最大值" }, { type: "min", name: "最小值" }]
+          },
+          markLine: {
+            data: [{ type: "average", name: "平均值" }]
+          }
+        }
+      ]
+    };
+  },
+  methods: {
+    // 初始化数据
+    init(params) {
+      this.mockChartData(params);
+      this.showChart(this.initTab);
+    },
+    // 分页数据(ajax异步获取，一次性获取完毕)
+    mockChartData(params) {
+      let ajaxData = [];
+    //   this.optionData[0].data = [520, 532, 601, 534, 790, 830, 310];
+      for (let index = 0; index < 7; index++) {
+          let element = Math.floor(Math.random() * 1000 + Math.random() * 10);
+          console.log();
+            this.optionData[0].data.push(element);
+      }
+      this.optionData[1].data = [820, 645, 546, 745, 872, 624, 258];
+      // //异步获取数据
+      //   util
+      //     .ajaxPost("", params)
+      //     .then(
+      //       function(response) {
+      //         // 遍历结果数据
+      //         if (response.data.data.length > 0) {
+      //           response.data.data.forEach(function(item) {
+      //             ajaxData.push({
+      //               name: item.name + Math.floor(Math.random() * 7 + 1),
+      //               site: item.site,
+      //               api_url: item.api_url,
+      //               status: Math.floor(Math.random() * 3 + 1),
+      //               portrayal: [
+      //                 "City",
+      //                 "People",
+      //                 "Cost",
+      //                 "Life",
+      //                 "Entertainment"
+      //               ],
+      //               people: [{}],
+      //               time: Math.floor(Math.random() * 7 + 1),
+      //               update: new Date()
+      //             });
+      //           });
+      //         }
+      //         // 赋值给当前的表格
+      //         this.optionData = ajaxData;
+      //       }.bind(this)
+      //     )
+      //     .catch(function(error) {
+      //       console.log(error);
+      //     });
+    },
+    // 图表展示
+    showChart(chartID) {
+      console.log(chartID);
+      const option = {
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#6a7985"
+            }
+          }
+        },
+        legend: {
+          data: this.optionLenged //标注
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            dataView: { show: true, readOnly: false },
+            magicType: { show: true, type: ["line", "bar"] },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        calculable: true,
+        grid: {
+          left: "1.2%",
+          right: "1%",
+          bottom: "3%",
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: "category",
+            boundaryGap: false,
+            data: this.optionXAxis //x轴
+          }
+        ],
+        yAxis: [
+          {
+            type: this.optionyAxis //y轴
+          }
+        ],
+        series: this.optionData //核心数据
+      };
+
+      this.chartObj = echarts.init(
+        document.getElementById(chartID),
+        "macarons"
+      );
+      // 3.0版本里的加载图示
+      this.chartObj.showLoading({
+        text: "正在努力的读取数据中...",
+        effect: "default", //'spin' | 'bar' | 'ring' | 'whirling' | 'dynamicLine' | 'bubble',
+        maskColor: "#E0E0E0",
+        color: "#c23531",
+        textColor: "#000"
+      });
+
+      setTimeout(() => {
+        this.chartObj.hideLoading();
+        this.chartObj.setOption(option);
+        this.chartObj.resize();
+      }, 2000);
+    },
+    // 处理tab切换
+    handleTabChange(name) {
+      let chartID = "key_request_con";
+      if (name > 0) {
+        chartID = "key_request_con" + name;
+      }
+
+      this.showChart(chartID);
     }
+  },
+  mounted() {
+    let params = {
+      cmdId: "getKeyRequest", //获取关键数据指标
+      gameId: localStorage.gameId
+    };
+    //初始化
+    this.init(params);
+  }
 };
 </script>
--- src/views/sumary/components/onlineRequests.vue
+++ src/views/sumary/components/onlineRequests.vue
@@ -1,126 +1,111 @@
 <template>
-  <div style="width:100%;height:100%;" id="online_request_con"></div>
+  <div class="echart_tab_style" style="width:100%;height:100%;" id="online_request_con"></div>
 </template>
 
 <script>
-const echarts = require('echarts');
-require('@/libs/echarts/macarons');
+const echarts = require("echarts");
+require("@/libs/echarts/macarons");
 export default {
-    name: 'onlineRequests',
-    mounted () {
-        const option = {
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'cross',
-                    label: {
-                        backgroundColor: '#6a7985'
-                    }
-                }
-            },
-            legend: {
-                data:['运营商/网络服务','银行/证券','游戏/视频','餐饮/外卖','快递/电商']
-            },
-            toolbox: {
-                show : true,
-                feature : {
-                    dataView : {show: true, readOnly: false},
-                    magicType : {show: true, type: ['line','bar']},
-                    restore : {show: true},
-                    saveAsImage : {show: true}
-                }
-            },
-            calculable : true,
-            grid: {
-                left: '1.2%',
-                right: '1%',
-                bottom: '3%',
-                containLabel: true
-            },
-            xAxis: [
-                {
-                    type: 'category',
-                    boundaryGap: false,
-                    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-                }
-            ],
-            yAxis: [
-                {
-                    type: 'value'
-                }
-            ],
-            series: [
-                {
-                    name: '运营商/网络服务',
-                    type: 'line',
-                    stack: '总量',
-                    areaStyle: {normal: {
-                        color: '#2d8cf0'
-                    }},
-                    data: [120, 132, 101, 134, 90, 230, 310]
-                },
-                {
-                    name: '银行/证券',
-                    type: 'line',
-                    stack: '总量',
-                    areaStyle: {normal: {
-                        color: '#10A6FF'
-                    }},
-                    data: [257, 358, 278, 234, 290, 330, 310]
-                },
-                {
-                    name: '游戏/视频',
-                    type: 'line',
-                    stack: '总量',
-                    areaStyle: {normal: {
-                        color: '#0C17A6'
-                    }},
-                    data: [379, 268, 354, 269, 310, 478, 358]
-                },
-                {
-                    name: '餐饮/外卖',
-                    type: 'line',
-                    stack: '总量',
-                    areaStyle: {normal: {
-                        color: '#4608A6'
-                    }},
-                    data: [320, 332, 301, 334, 390, 330, 320]
-                },
-                {
-                    name: '快递/电商',
-                    type: 'line',
-                    stack: '总量',
-                    label: {
-                        normal: {
-                            show: true,
-                            position: 'top'
-                        }
-                    },
-                    areaStyle: {normal: {
-                        color: '#398DBF'
-                    }},
-                    data: [820, 645, 546, 745, 872, 624, 258],
-                    markPoint : {
-                        data : [
-                            {type : 'max', name: '最大值'},
-                            {type : 'min', name: '最小值'}
-                        ]
-                    },
-                    markLine : {
-                        data : [
-                            {type : 'average', name: '平均值'}
-                        ]
-                    }
-                }
-            ]
-        };
-        const serviceRequestCharts = echarts.init(document.getElementById('online_request_con'),'macarons');
-        serviceRequestCharts.setOption(option);
+  name: "onlineRequests",
+  mounted() {
+    const option = {
+      // title: {
+      //   text: "ACU & PCU",
+      //   left: "center"
+      // },
+      //   color: ["rgba(255, 99, 0, 0.5)", "rgba(55, 200, 100, 0.5)"],
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "cross",
+          label: {
+            backgroundColor: "#6a7985"
+          }
+        }
+      },
+      legend: {
+        data: ["ACU", "PCU"]
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          dataView: { show: true, readOnly: false },
+          magicType: { show: true, type: ["line", "bar"] },
+          restore: { show: true },
+          saveAsImage: { show: true }
+        }
+      },
+      calculable: true,
+      grid: {
+        left: "1.2%",
+        right: "1%",
+        bottom: "3%",
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: "category",
+          boundaryGap: false,
+          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
+        }
+      ],
+      yAxis: [
+        {
+          type: "value"
+        }
+      ],
+      series: [
+        {
+          name: "ACU",
+          type: "line",
+          stack: "总量",
+          itemStyle: { normal: { areaStyle: { type: "default" } } },
+          data: [520, 532, 601, 534, 790, 830, 310]
+        },
+        {
+          name: "PCU",
+          type: "line",
+          stack: "总量",
+          label: {
+            normal: {
+              show: true,
+              position: "top"
+            }
+          },
+          itemStyle: { normal: { areaStyle: { type: "default" } } },
+          data: [820, 645, 546, 745, 872, 624, 258],
+          markPoint: {
+            data: [{ type: "max", name: "最大值" }, { type: "min", name: "最小值" }]
+          },
+          markLine: {
+            data: [{ type: "average", name: "平均值" }]
+          }
+        }
+      ]
+    };
 
+    const serviceRequestCharts = echarts.init(
+      document.getElementById("online_request_con"),
+      "macarons"
+    );
+    // 3.0版本里的加载图示
+    serviceRequestCharts.showLoading({
+      text: "正在努力的读取数据中...",
+      effect: "default", //'spin' | 'bar' | 'ring' | 'whirling' | 'dynamicLine' | 'bubble',
+      maskColor: "#E0E0E0",
+      color: "#c23531",
+      textColor: "#000"
+    });
+    let loadingTicket = 0;
+    clearTimeout(loadingTicket);
+    loadingTicket = setTimeout(function() {
+      serviceRequestCharts.hideLoading();
+      serviceRequestCharts.setOption(option);
 
-        window.addEventListener('resize', function () {
-            serviceRequestCharts.resize();
-        });
-    }
+      window.addEventListener("resize", function() {
+        serviceRequestCharts.resize();
+      });
+    }, 2000);
+  }
 };
 </script>
